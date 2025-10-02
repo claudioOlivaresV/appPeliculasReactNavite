2db2f20f711fc4d846f91e9d39b1e61fe8359ad9
Claudio Olivares <ca.olivares91@gmail.com>
2025-10-02 18:01:01 -0300

    estructura app

---
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export const DetailsScreen = () => {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+};
